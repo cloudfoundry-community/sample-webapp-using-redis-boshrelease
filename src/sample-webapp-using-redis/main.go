f6265f2e24bdd2ee8236510225e71991705cbc90
Dr Nic Williams <drnicwilliams@gmail.com>
2019-11-16 19:56:50 +1000

    sample webapp increments and returns a counter

--- src/sample-webapp-using-redis/main.go
+++ src/sample-webapp-using-redis/main.go
@@ -28,6 +28,10 @@ func main() {
 	if redisDBNum == "" {
 		redisDBNum = "0"
 	}
+	redisCounterKey := os.Getenv("REDIS_COUNTER_KEY")
+	if redisCounterKey == "" {
+		redisCounterKey = "counter"
+	}
 	redisDB, _ := strconv.ParseInt(redisDBNum, 10, 32)
 	redisPassword := os.Getenv("REDIS_PASSWORD")
 	redisAddr := fmt.Sprintf("%s:%s", redisHost, redisPort)
@@ -40,5 +44,18 @@ func main() {
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		val, err := client.Get(redisCounterKey).Int()
+		if err == redis.Nil {
+			// defaults to 0
+		} else if err != nil {
+			fmt.Fprintf(w, "Cannot connect to Redis backend\n")
+		}
+		val++
+		client.Set(redisCounterKey, strconv.Itoa(val), 0)
+		fmt.Fprintf(w, "%s=%d\n", redisCounterKey, val)
+	})
+
+	fmt.Printf("Web app listening on :%s...\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
